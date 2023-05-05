8c4c9138b425a9209406c5febc6d6e3e4116d073
Daniel Seese <daniel.seese@iamrobotics.com>
2023-05-05 10:15:22 -0400

    removed history

--- .history/headless_20230504094404.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-chromeOptions = Options()
-chromeOptions.headless = True
-browser = webdriver.Chrome(executable_path="/usr/local/bin/", options=chromeOptions)
-browser.get("http://linuxhint.com")
-print("Title: %s" % browser.title)
-browser.quit()
--- .history/headless_20230504094627.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-chromeOptions = Options()
-chromeOptions.headless = True
-browser = webdriver.Chrome(executable_path="/home/daniel.seese/Downloads/", options=chromeOptions)
-browser.get("http://linuxhint.com")
-print("Title: %s" % browser.title)
-browser.quit()
